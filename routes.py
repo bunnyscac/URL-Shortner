a28b6f757aeb16bf8d97b5af3542ed88fa239a72
Niket <58136275+bunnyscac@users.noreply.github.com>
2021-06-11 23:43:31 +0530

    ADDED ROUTES

--- routes.py
+++ routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
+from .extensions import db
+from .models import Link
+
 
 shortner = Blueprint('shortner', __name__)
 
@@ -6,3 +9,29 @@ shortner = Blueprint('shortner', __name__)
 @shortner.route('/<short_url>')
 def redirect_url_to(short_url):
     return ""
+
+
+@shortner.route('/create_link', methods=["POST"])
+def create_link():
+    original_url = request.form['original_url']
+    link = Link(original_url=original_url)
+    db.session.add(link)
+    db.session.commit()
+
+    return render_template('link_success.html',
+                           new_url=link.short_url, original_url=link.original_url)
+
+
+@shortner.route('/')
+def index():
+    return render_template("index.html")
+
+
+@shortner.route('/analytics')
+def analytics():
+    return ""
+
+
+@shortner.errorhandler(404)
+def page_not_found():
+    return '<h1>WOAPS! PAGE NOT FOUND</h1>', 404
